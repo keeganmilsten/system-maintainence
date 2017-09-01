fb1b51b069a60da100ca593f6461cf6ca2d1a899
Keegan <willnhalt@gmail.com>
2017-09-01 10:52:59 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+su
+mv /home/$USER/system-maintainance/clean.sh /usr/bin/
+mv /home/$USER/system-maintainance/update.sh /usr/bin/
+mv /home/$USER/system-mainainance/clean2.sh /usr/bin/
+mv /home/$USER/system-maintainance/system-maintainance.desktop /home/$USER/.local/share/
+cd
+cd /usr/bin
+chmod +x /usr/bin/clean.sh
+chmod +x /usr/bin/update.sh
+chmod +x /usr/bin/clean2.sh
