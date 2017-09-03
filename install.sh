27729b142c110f3a59e0735165af2f2d1365fd51
Keegan <willnhalt@gmail.com>
2017-09-03 15:28:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@ su
 mv /home/$USER/system-maintainance/clean.sh /usr/bin/
 mv /home/$USER/system-maintainance/update.sh /usr/bin/
 mv /home/$USER/system-mainainance/clean2.sh /usr/bin/
-mv /home/$USER/system-maintainance/system-maintainance.desktop /home/$USER/.local/share/
+mv /home/$USER/system-mainainence/remove.sh /usr/bin/
+mv /home/$USER/system-maintainance/system-maintainance.desktop /home/$USER/.local/share/applications/
 cd
 cd /usr/bin
 chmod +x /usr/bin/clean.sh
 chmod +x /usr/bin/update.sh
 chmod +x /usr/bin/clean2.sh
+chmod +x /usr/bin/remove.sh
